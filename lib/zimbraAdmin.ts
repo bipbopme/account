00f79887286be18a73622c0676213c30dce5c380
Nick Chapman <nchapman@gmail.com>
2020-06-15 14:57:46 -0700

    Revert "Just auth on every request for now."
    This reverts commit c1c337f643bcc8539cc654bd8f57e23d0b0449d5.

--- lib/zimbraAdmin.ts
+++ lib/zimbraAdmin.ts
@@ -7,18 +7,20 @@ export default class ZimbraAdmin {
   }
 
   async auth(username: string, password: string): Promise<void> {
-    const response = await this.post({
-      AuthRequest: {
-        _jsns: "urn:zimbraAdmin",
-        account: {
-          by: "name",
-          _content: username,
+    if (!this.authToken) {
+      const response = await this.post({
+        AuthRequest: {
+          _jsns: "urn:zimbraAdmin",
+          account: {
+            by: "name",
+            _content: username,
+          },
+          password: password,
         },
-        password: password,
-      },
-    });
+      });
 
-    this.authToken = response?.Body?.AuthResponse?.authToken[0]?._content;
+      this.authToken = response?.Body?.AuthResponse?.authToken[0]?._content;
+    }
   }
 
   async createAccount(
